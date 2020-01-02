aeaf158df6696e7e5e6e0f54159ca9c4e3eca6e1
Alex Engelhart <asengelhart@yahoo.com>
2020-01-02 20:18:44 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -35,7 +35,7 @@ class Waiter
   def best_average_tipper
     tips_by_customer = meals.inject({}) do |memo, meal|
       customer = meal.customer 
-      binding.pry
+
       if memo[customer] == nil 
         memo[customer] = []
       else 
@@ -46,6 +46,7 @@ class Waiter
       memo[customer] = tips.reduce(){|memo, tip| memo += tip}
       memo[customer] /= tips.size 
     end 
+    binding.pry
     averages_by_customer.max{|cust_a, cust_b| cust_a.value <=> cust_b.value}.key
   end 
   
