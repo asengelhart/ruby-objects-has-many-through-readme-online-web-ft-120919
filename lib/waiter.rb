6d7b1fc86bc2ea3c20ecd11d846c20b386472f3c
Alex Engelhart <asengelhart@yahoo.com>
2020-01-02 19:48:01 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -15,4 +15,30 @@ class Waiter
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end 
+  
+  def meals 
+    Meal.all.select do |meal|
+      meal.waiter == self 
+    end 
+  end 
+  
+  def best_tipper 
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip 
+    end
+    best_tipped_meal.customer 
+  end 
+  
+  #Just for funsies
+  def best_average_tipper
+    tips_by_customer = meals.inject({}) do |memo, meal|
+      customer = meal.customer 
+      if memo[customer] == nil 
+        memo[customer] = []
+      else 
+        memo[customer] << meal.tip 
+      end 
+      
+    end 
+  end 
 end
